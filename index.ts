088a9630b90fde0d9182831d0819076b9e958daa
Usama Sheikh <usamasheikhoffical786@gmail.com>
2024-04-12 16:00:52 +0500

    push my numberGuess game to github

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"; 
+
+const randomNum = Math.floor(Math.random())*6 + 1;
+const answer = await inquirer.prompt([
+    {
+    message:"Please guess a game number : 1-6",
+    type:"number",
+    name:"userNumber",
+}])
+
+if(answer.userNumber !== randomNum){
+    console.log("You Guess a wrong number:Guess again");
+}else{
+    console.log("Congratulation you enter a right number");
+}
